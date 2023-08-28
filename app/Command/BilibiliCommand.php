98bd499533d06a0178f72cd061f9efa88daa67cb
fly321 <luoliyaa@hotmail.com>
2023-08-28 08:39:51 +0800

    错误的案例

--- app/Command/BilibiliCommand.php
+++ app/Command/BilibiliCommand.php
@@ -10,6 +10,7 @@ use Hyperf\Command\Command as HyperfCommand;
 use Hyperf\Command\Annotation\Command;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\Event\EventDispatcher;
+use Hyperf\Pool\Channel;
 use Psr\Container\ContainerInterface;
 use Swoole\Event;
 
@@ -24,6 +25,7 @@ class BilibiliCommand extends HyperfCommand
     #[Inject]
     protected EventDispatcher $event;
 
+
     public function __construct(protected ContainerInterface $container)
     {
         parent::__construct('bilibili:clock_in');
@@ -37,27 +39,31 @@ class BilibiliCommand extends HyperfCommand
 
     public function handle()
     {
+
         date_default_timezone_set('Asia/Shanghai');
         $this->line("bilibili:clock_in running...", "info", true);
-        \Swoole\Coroutine\run(function (){
-            // 每天0点执行
-            $this->timer->tick(86400000, function (){
-                $this->line("开始执行", "info", true);
-                $this->line("当前时间".date("Y-m-d H:i:s"));
-                $this->bilibiliService->setCookie();
-                $this->line("获取cookie成功：".$this->bilibiliService->getCookie(), "info", true);
-                $data = $this->bilibiliService->listOfFanCards();
-                $this->line("获取粉丝勋章列表成功:".json_encode($data), "info", true);
-                $this->line("开始签到", "info", true);
-                $jct = $this->bilibiliService->getJct();
-                foreach ($data as $item) {
-                    sleep(5);
-                    $room_id = $this->bilibiliService->getRoomId($item["medal_info"]['target_id']);
-                    // 获取房间号 ， 签到,写入日志
-                    $this->bilibiliService->clockIn($room_id, $jct);
-                    $this->line("当前时间".date("Y-m-d H:i:s").":"."{$item['target_name']} 签到成功", "info", true);
-                }
-            });
+
+        $channel = new Channel(1);
+        // 每天0点执行
+        $this->timer->tick(86400000, function (){
+            $this->line("开始执行", "info", true);
+            $this->line("当前时间".date("Y-m-d H:i:s"));
+            $this->bilibiliService->setCookie();
+            $this->line("获取cookie成功：".$this->bilibiliService->getCookie(), "info", true);
+            $data = $this->bilibiliService->listOfFanCards();
+            $this->line("获取粉丝勋章列表成功:".json_encode($data), "info", true);
+            $this->line("开始签到", "info", true);
+            $jct = $this->bilibiliService->getJct();
+            foreach ($data as $item) {
+                sleep(5);
+                $room_id = $this->bilibiliService->getRoomId($item["medal_info"]['target_id']);
+                // 获取房间号 ， 签到,写入日志
+                $this->bilibiliService->clockIn($room_id, $jct);
+                $this->line("当前时间".date("Y-m-d H:i:s").":"."{$item['target_name']} 签到成功", "info", true);
+            }
         });
+
+        $channel->pop(0);
+
     }
 }
