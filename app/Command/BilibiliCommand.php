c52db0f521efe7d6c471feea5fe1ef43f32cc21d
fly321 <luoliyaa@hotmail.com>
2023-08-28 08:17:30 +0800

    1

--- app/Command/BilibiliCommand.php
+++ app/Command/BilibiliCommand.php
@@ -36,9 +36,8 @@ class BilibiliCommand extends HyperfCommand
     {
         date_default_timezone_set('Asia/Shanghai');
         $this->line("bilibili:clock_in running...", "info", true);
-
         // 每天0点执行
-        $this->timer->tick(86400000, function (){
+        \Swoole\Timer::tick(86400000, function (){
             $this->line("开始执行", "info", true);
             $this->line("当前时间".date("Y-m-d H:i:s"));
             $this->bilibiliService->setCookie();
